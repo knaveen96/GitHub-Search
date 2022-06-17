556796207aadd32e2d155f00574d0a1b4f7f095d
Naveen K <naveen.ktf23@gmail.com>
2022-06-17 19:36:18 -0400

    Pie chart is now dynamic !

--- components/Repos.js
+++ components/Repos.js
@@ -3,6 +3,26 @@ import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
+
+  const { repos } = React.useContext(GithubContext)
+
+  let languages = repos.reduce((total, repo) => {
+    const { language } = repo
+    if (!language) return total
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 }
+    } else {
+      total[language] = { ...total[language], value: total[language].value + 1 }
+    }
+    return total
+  }, {})
+
+  languages = Object.values(languages).sort((a, b) => {
+    return b.value - a.value
+  }).slice(0, 5)
+
+  console.log(languages)
+
   const chartData = [
     {
       label: "HTML",
@@ -25,7 +45,7 @@ const Repos = () => {
   return <section className='section'>
     <Wrapper className='section-center'>
       {/* <ExampleChart data={chartData} /> */}
-      <Pie3D data={chartData} />
+      <Pie3D data={languages} />
     </Wrapper>
   </section>
 };
